0aa8eed9a8e158128d9c9a56f634c3cd9381ce93
Kishan Ayyangar <111353230+Kishanes@users.noreply.github.com>
2023-01-10 17:19:27 +0530

    Add files via upload
    Call Back Hell Task

---
+++ script.js
@@ -0,0 +1,66 @@
+const heading = document.createElement('h2');
+const headingTagls = document.createElement('h2');
+document.body.style.textAlign = 'center';
+document.body.style.backgroundColor = 'white';
+document.body.style.color = 'Orange';
+document.body.style.marginTop = '300px';
+document.body.style.fontSize = "x-large";
+document.body.append(heading);
+document.body.append(headingTagls);
+var countDown = 10;
+setTimeout(() => {
+    heading.innerText = '';
+    heading.innerText = countDown;
+    countDown--;
+    setTimeout(() => {
+        heading.innerText = '';
+        heading.innerText = countDown;
+        countDown--;
+        setTimeout(() => {
+            heading.innerText = '';
+            heading.innerText = countDown;
+            countDown--;
+            setTimeout(() => {
+                heading.innerText = '';
+                heading.innerText = countDown;
+                countDown--;
+                    setTimeout(() => {
+                    heading.innerText = '';
+                    heading.innerText = countDown;
+                    countDown--;
+                        setTimeout(() => {
+                        heading.innerText = '';
+                        heading.innerText = countDown;
+                        countDown--;
+                        setTimeout(() => {
+                            heading.innerText = '';
+                            heading.innerText = countDown;
+                            countDown--;
+                            setTimeout(() => {
+                            heading.innerText = '';
+                            heading.innerText = countDown;
+                            countDown--;
+                            setTimeout(() => {
+                                heading.innerText = '';
+                                heading.innerText = countDown;
+                                countDown--;
+                                setTimeout(() => {
+                                    heading.innerText = '';
+                                    heading.innerText = countDown;
+                                    countDown--;
+                                    setTimeout(() => {
+                                        heading.innerText = '';
+                                        headingTagls.innerText = 'Happy Independence Day!';
+                                    countDown--;
+                                }, 900);
+                            }, 900);
+                            }, 900);
+                        }, 900);
+                        }, 900);
+                    }, 900);
+                }, 900);
+            }, 900);
+        }, 900);
+    }, 900);
+}, 900);
+
